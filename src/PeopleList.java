91c5a07beef965b0e34b135963a7f10bbb042d13
Emerson Leite <emerson.bl@gmail.com>
2025-03-08 21:30:44 -0300

    chore: update PeopleList

--- src/PeopleList.java
+++ src/PeopleList.java
@@ -1,69 +1,85 @@
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+
+import comparators.HeightComparator;
 import entities.People;
 
+@SuppressWarnings("SpellCheckingInspection")
 public class PeopleList {
 
-  List<People> peopleList = null;
+    List<People> peopleList;
 
-  public PeopleList() {
-    this.peopleList = new ArrayList<>();
-  }
-
-  public void addPerson(People person) {
-    this.peopleList.add(person);
-  }
-
-  public void viewList() {
-    System.out.println(this.peopleList);
-  }
-
-
-  private boolean isListEmpty(){
-    if(this.peopleList.isEmpty()) {
-      System.out.println("a lista está vazia");
-    }
-    return this.peopleList.isEmpty();
-  }
-
-  public void removeOne(People p1) {
-    if(isListEmpty()){
-      return;
-    }
-    this.peopleList.remove(p1);
-  }
-
-  public void removeByName(String name){
-    if(isListEmpty()){
-      return;
+    public PeopleList() {
+        this.peopleList = new ArrayList<People>();
     }
 
-    List<People> listToExclude = new ArrayList<>();
+    public void addPerson(People person) {
+        this.peopleList.add(person);
+    }
 
-      for(People p: this.peopleList){
-        if(p.getName().equalsIgnoreCase(name)){
-          listToExclude.add(p);
+    public void viewList() {
+        System.out.println(this.peopleList);
+    }
+
+    private boolean isListEmpty() {
+        if (this.peopleList.isEmpty()) {
+            System.out.println("a lista está vazia");
         }
-      }
-      this.peopleList.removeAll(listToExclude);
-  }
-
-  public List<People> findByName(String name){
-    List<People> list = new ArrayList<>();
-
-    for(People p: this.peopleList){
-      if(p.getName().equalsIgnoreCase(name)){
-        list.add(p);
-      }
+        return this.peopleList.isEmpty();
     }
-    return list;
-  }
 
-  public void removeMany(List<People> peopleList){
-    if(isListEmpty()){
-      return;
+    public void removeOne(People p1) {
+        if (isListEmpty()) {
+            return;
+        }
+        this.peopleList.remove(p1);
     }
-    this.peopleList.removeAll(peopleList);
-  }
+
+    public void removeByName(String name) {
+        if (isListEmpty()) {
+            return;
+        }
+
+        List<People> listToExclude = new ArrayList<>();
+
+        for (People p : this.peopleList) {
+            if (p.getName().equalsIgnoreCase(name)) {
+                listToExclude.add(p);
+            }
+        }
+        this.peopleList.removeAll(listToExclude);
+    }
+
+    public void removeMany(List<People> peopleList) {
+        if (isListEmpty()) {
+            return;
+        }
+        this.peopleList.removeAll(peopleList);
+    }
+
+    public List<People> findByName(String name) {
+        List<People> list = new ArrayList<>();
+
+        for (People p : this.peopleList) {
+            if (p.getName().equalsIgnoreCase(name)) {
+                list.add(p);
+            }
+        }
+        return list;
+    }
+
+    public List<People> ordenateByAge() {
+        List<People> list = new ArrayList<>(peopleList);
+        Collections.sort(list);
+        return list;
+    }
+
+    /* Utilizando uma Classe que extende Comparator */
+    public List<People> ordenateByHeight() {
+        List<People> list = new ArrayList<>(peopleList);
+        Collections.sort(list, new HeightComparator());
+        return list;
+
+    }
 }
