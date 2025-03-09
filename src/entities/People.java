9bce845bef700a07cbacf0152e2b9c74e326f74f
Emerson Leite <emerson.bl@gmail.com>
2025-03-08 21:31:44 -0300

    chore: update People

--- src/entities/People.java
+++ src/entities/People.java
@@ -1,66 +1,63 @@
 package entities;
 
-public class People {
+public class People implements Comparable<People> {
 
-  private String name;
-  private int age;
-  private String job;
+    private String name;
+    private int age;
+    private String job;
+    private float height;
 
-  public People(String name, int age, String job) {
-    this.name = name;
-    this.age = age;
-    this.job = job;
-  }
+    public People(String name, int age, String job, float height) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.height = height;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public int getAge() {
-    return age;
-  }
+    public int getAge() {
+        return age;
+    }
 
-  public String getJob() {
-    return job;
-  }
+    public float getHeight() {
+        return height;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    @Override
+    public String toString() {
+        return "name=" + name + ", age=" + age + ", job=" + job + ", height=" + height;
+    }
 
-  public void setAge(int age) {
-    this.age = age;
-  }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
 
-  public void setJob(String job) {
-    this.job = job;
-  }
+    /* Permite a utilização de Collections.sort para lista */
+    @Override
+    public int compareTo(People o) {
+        return Integer.compare(age, o.getAge());
+    }
 
-  @Override
-  public String toString() {
-    return "name=" + name + ", age=" + age + ", job=" + job;
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((name == null) ? 0 : name.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    People other = (People) obj;
-    if (name == null) {
-        return other.name == null;
-    } else return name.equals(other.name);
-  }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        People other = (People) obj;
+        if (name == null) {
+            return other.name == null;
+        } else
+            return name.equals(other.name);
+    }
 
 }
