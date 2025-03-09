9a0ecac4f6c8f86c970999cf6bd3c0c8f2330d34
Emerson Leite <emerson.bl@gmail.com>
2025-03-08 21:31:26 -0300

    chore: create HeightComparator

---
+++ src/comparators/HeightComparator.java
@@ -0,0 +1,13 @@
+package comparators;
+
+import java.util.Comparator;
+import entities.People;
+
+public class HeightComparator implements Comparator<People> {
+
+  @Override
+  public int compare(People p1, People p2) {
+    return Float.compare(p1.getHeight(), p2.getHeight());
+  }
+
+}
